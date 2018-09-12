9ea9949c8d73f8732a991626bf4ea32e564d95a0
Gotte Alexandre <alexandre.gotte@riminder.net>
2018-09-12 13:02:29 +0200

    Added gemspec

--- lib/not_a_main.rb
+++ lib/not_a_main.rb
@@ -1,63 +1,7 @@
-require 'rest-client'
-require 'json'
-
 require_relative './riminder.rb'
 
-def gen_profilejson()
-    pjson = {
-        "name" => 'Magalie Desktest',
-        "email" => 'magalie.desktest@gmail.com',
-        "phone" => '+33743457894',
-        "summary" => 'I the world greatest philosopher',
-        "location_details" => {
-            "text" => "Moon"
-        },
-        "experiences" => [
-            {
-                "start" => "23/05/2010",
-                "end" => "",
-                "title" => "The Philosopher",
-                "company" => "Moon thinker",
-                "location" => nil,
-                "location_details" => {
-                    "text" => "Moon"
-                },
-                "description" => "Chief of all philosophers"
-            }
-        ],
-        "educations" => [
-            {
-                "start" => "23/05/2005",
-                "end" => "2/04/2010",
-                "title" => "Philosophy",
-                "school" => "Moon school",
-                "location" => nil,
-                "location_details" => {
-                    "text" => "Moon"
-                },
-                "description" => ""
-            }
-        ],
-        "skills" => ["diplomacy", "Politics", "speech"],
-        "languages" => ["english", "chinese", "french", "Swedish"],
-        "interests" => ["jumping", "speaking", "video games", "sports"],
-        "urls" => {
-            "from_resume" => [],
-            "linkedin" => nil,
-            "twitter" => nil,
-            "facebook" => nil,
-            "github" => nil,
-            "picture" => nil
-        }
-    }
-    return pjson
-end
+api = Riminder.new("haha")
 
-api = Riminder.new("ask_874138568ebde822652c3ddf2218333a")
-
-resp = api.profile.stage.set "source_id" => "fe6d7a2aa9125259a5ecf7905154a0396a891c06", 
-    "profile_id" => "be50e7e57f4ad045ac8c406e7b665d409be9e363",
-    "filter_id" => "050bdaa9cedad3cf2bc2fac3e4e4acb7499d762a", "stage" => "NO"
 resp = api.filter.list()
 puts(resp)
 
---
+++ riminder.gemspec
@@ -0,0 +1,16 @@
+Gem::Specification.new do |s|
+  s.name        = 'Riminder'
+  s.version     = '0.0.1'
+  s.licenses    = ['MIT']
+  s.summary     = "A ruby api client for Riminder api."
+  s.description = "A api client for riminder, permit to access and manipulate some Riminder's services without the web interface."
+  s.authors     = ["Gotte Alexandre"]
+  s.email       = 'contact@riminder.net'
+  s.files       = ["lib/filter.rb", "lib/profile.rb", "lib/requtils.rb", "lib/restClientW.rb", "lib/riminderException.rb", "lib/riminder.rb", "lib/source.rb", "lib/webhook.rb"]
+  s.homepage    = 'https://riminder.net'
+  s.metadata    = { "source_code_uri" => "https://github.com/Riminder/ruby-riminder-api" }
+  s.add_runtime_dependency 'rest-client', '~> 2.0', '>= 2.0.2'
+  s.add_runtime_dependency 'activesupport', '~> 5.0'
+  s.add_development_dependency 'rubocop', '~> 0.58.2'
+  s.add_development_dependency 'rspec', '~> 3.5'
+end
